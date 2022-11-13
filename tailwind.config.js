6cfc3f114238167a7e844dca68fcf7e4af3588ec
Akshith <garapatiakshith@gmail.com>
2022-11-13 21:39:08 +0530

    feat: spotify status + WIP alert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        krona: ['Krona One'],
+      },
+    },
   },
   plugins: [],
-}
+};
