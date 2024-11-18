256ddb5bad924ed71d748c373fdc012d8f89455b
Akshith <garapatiakshith@gmail.com>
2024-11-18 18:22:00 -0500

    feat: preliminary mdx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,12 +38,22 @@ export default function HomePage() {
             ? song.substring(0, song.indexOf("(")).trim()
             : song;
 
-          const artist = data.data.spotify.artist
+          let artist = data.data.spotify.artist
             .replaceAll(";", ",")
             .toLowerCase()
             .split(",")
             .slice(0, 2)
             .join(",");
+
+          let songString = songName + artist + "  •  ";
+
+          if (songString.length > 55) {
+            artist = data.data.spotify.artist
+              .replaceAll(";", ",")
+              .toLowerCase()
+              .split(",")
+              .slice(0, 1);
+          }
 
           setSpotify(`${songName}1xe34${artist}`);
         } else {
@@ -143,7 +153,7 @@ export default function HomePage() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <g clip-path="url(#clip0_997_198)">
+            <g clipPath="url(#clip0_997_198)">
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -196,7 +206,7 @@ export default function HomePage() {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g clip-path="url(#clip0_997_197)">
+              <g clipPath="url(#clip0_997_197)">
                 <path
                   d="M5.03344 14.5C11.0697 14.5 14.3722 9.4978 14.3722 5.16124C14.3722 5.02062 14.3691 4.87687 14.3628 4.73624C15.0052 4.27164 15.5597 3.69617 16 3.03687C15.4017 3.30307 14.7664 3.47692 14.1159 3.55249C14.8009 3.14194 15.3137 2.49698 15.5594 1.73718C14.915 2.11904 14.2104 2.38841 13.4756 2.53374C12.9806 2.00771 12.326 1.65941 11.6131 1.5427C10.9003 1.42599 10.1688 1.54737 9.53183 1.88806C8.89486 2.22876 8.38787 2.7698 8.08923 3.42754C7.7906 4.08528 7.71695 4.82308 7.87969 5.52687C6.575 5.46139 5.29862 5.12247 4.13332 4.53207C2.96802 3.94166 1.9398 3.11296 1.11531 2.09968C0.696266 2.82216 0.568038 3.6771 0.756687 4.49073C0.945337 5.30436 1.43671 6.01563 2.13094 6.47999C1.60975 6.46344 1.09998 6.32312 0.64375 6.07062V6.11124C0.643283 6.86943 0.905399 7.60439 1.38554 8.19118C1.86568 8.77797 2.53422 9.18037 3.2775 9.32999C2.7947 9.46209 2.28799 9.48133 1.79656 9.38624C2.0063 10.0383 2.41438 10.6086 2.96385 11.0176C3.51331 11.4265 4.17675 11.6537 4.86156 11.6675C3.69895 12.5807 2.26278 13.0761 0.784375 13.0737C0.522191 13.0733 0.260266 13.0573 0 13.0256C1.5019 13.9892 3.24902 14.5009 5.03344 14.5Z"
                   fill="#999999"
@@ -292,7 +302,7 @@ export default function HomePage() {
               </div>
 
               <div
-                className={`${duplet.className} mt-[26px] w-[332px] text-[16px] leading-6 text-[#111] dark:text-[#eee]`}
+                className={`${duplet.className} relative mt-[26px] w-[332px] text-[16px] leading-6 text-[#111] dark:text-[#eee]`}
               >
                 <p>
                   I’m a comp sci (‘28) freshman{" "}
@@ -341,25 +351,26 @@ export default function HomePage() {
                   linktree replacement amongst many other random chronicles in
                   between.
                 </p>
-              </div>
 
-              {spotify !== "not listening to anything" && (
-                <div className="mt-[24px] w-full">
-                  <h1
-                    className={`${duplet.className} text-[16px] text-[#999] dark:text-[#999]`}
-                  >
-                    listening to{" "}
-                    {spotify.substring(0, spotify.indexOf("1xe34"))}
-                    {"  "}•{"  "}
-                    {spotify.substring(spotify.indexOf("1xe34") + 5)}
-                  </h1>
-                </div>
-              )}
+                {spotify !== "not listening to anything" && (
+                  <div className="absolute bottom-[-48px]">
+                    <h1
+                      className={`${duplet.className} whitespace-nowrap text-[16px] text-[#999] dark:text-[#999]`}
+                    >
+                      listening to{" "}
+                      {spotify.substring(0, spotify.indexOf("1xe34"))}
+                      {"  "}•{"  "}
+                      {spotify.substring(spotify.indexOf("1xe34") + 5)}{" "}
+                      {"            "}
+                    </h1>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="relative ml-[20%] grid w-[40%] place-items-center">
+        <div className="relative ml-[15%] grid w-[40%] place-items-center">
           <h1 className="text-[200px]">🍃</h1>
           <h1
             className={`${erika.className} absolute bottom-[20%] left-[15%] w-[145px] leading-6 text-[#111] dark:text-[#eee]`}
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
 import { duplet, erika, passenger } from "../../helpers/fonts";
 
 export default function HomePage() {
+
+  
+
   return (
     <body className="h-screen w-screen overflow-x-hidden overflow-y-hidden bg-[#eee] p-[24px]">
       <div
@@ -30,15 +32,6 @@ export default function HomePage() {
         </h1>
       </div>
 
-      <div className="absolute right-0 top-[10%]">
-        <Image
-          src="/image.png"
-          width={377}
-          height={812.5}
-          alt="background image of drawn gears"
-        />
-      </div>
-
       <div className="absolute bottom-[8px] left-[8px] flex items-center justify-center gap-x-[12px]">
         <svg
           width="16"
@@ -47,7 +40,7 @@ export default function HomePage() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g clip-path="url(#clip0_997_198)">
+          <g clipPath="url(#clip0_997_198)">
             <path
               fill-rule="evenodd"
               clip-rule="evenodd"
@@ -98,7 +91,7 @@ export default function HomePage() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <g clip-path="url(#clip0_997_197)">
+            <g clipPath="url(#clip0_997_197)">
               <path
                 d="M5.03344 14.5C11.0697 14.5 14.3722 9.4978 14.3722 5.16124C14.3722 5.02062 14.3691 4.87687 14.3628 4.73624C15.0052 4.27164 15.5597 3.69617 16 3.03687C15.4017 3.30307 14.7664 3.47692 14.1159 3.55249C14.8009 3.14194 15.3137 2.49698 15.5594 1.73718C14.915 2.11904 14.2104 2.38841 13.4756 2.53374C12.9806 2.00771 12.326 1.65941 11.6131 1.5427C10.9003 1.42599 10.1688 1.54737 9.53183 1.88806C8.89486 2.22876 8.38787 2.7698 8.08923 3.42754C7.7906 4.08528 7.71695 4.82308 7.87969 5.52687C6.575 5.46139 5.29862 5.12247 4.13332 4.53207C2.96802 3.94166 1.9398 3.11296 1.11531 2.09968C0.696266 2.82216 0.568038 3.6771 0.756687 4.49073C0.945337 5.30436 1.43671 6.01563 2.13094 6.47999C1.60975 6.46344 1.09998 6.32312 0.64375 6.07062V6.11124C0.643283 6.86943 0.905399 7.60439 1.38554 8.19118C1.86568 8.77797 2.53422 9.18037 3.2775 9.32999C2.7947 9.46209 2.28799 9.48133 1.79656 9.38624C2.0063 10.0383 2.41438 10.6086 2.96385 11.0176C3.51331 11.4265 4.17675 11.6537 4.86156 11.6675C3.69895 12.5807 2.26278 13.0761 0.784375 13.0737C0.522191 13.0733 0.260266 13.0573 0 13.0256C1.5019 13.9892 3.24902 14.5009 5.03344 14.5Z"
                 fill="#999999"
@@ -129,108 +122,7 @@ export default function HomePage() {
       </div>
 
       <div className="flex w-screen">
-        <div>
-          <div>
-            <div className="ml-[136px] mt-[58px]">
-              <h1 className={`${duplet.className} text-[16px] text-[#999]`}>
-                ☀️ i'm in west lafayette and it's 17:24
-              </h1>
 
-              <div className="mt-[12px] flex">
-                <svg
-                  width="32"
-                  height="35"
-                  viewBox="0 0 32 35"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.3138 15.4526C12.3138 18.1621 12.2715 21.4643 11.8481 24.8089C11.5518 27.1797 10.9591 29.4658 9.73132 31.5826C9.26562 32.3447 8.71526 33.0644 8.03788 33.6571C6.25977 35.1812 4.01597 35.1388 2.2802 33.5301C1.51815 32.8527 1.05246 32.006 0.713769 31.0746C0.29041 29.8468 0.121066 28.5344 0.0787304 27.222C-0.132949 22.3534 0.0787304 17.4847 0.756105 12.6584C1.01012 10.9227 1.34881 9.1869 1.94151 7.49347C2.36487 6.26572 2.91524 5.12265 3.67728 4.10659C4.43933 3.09053 5.41306 2.32848 6.68313 1.9898C7.27584 1.86279 7.8262 1.90512 8.41891 1.9898C10.0277 2.28615 10.9591 3.21754 11.4671 4.74163C11.9328 6.01171 12.1445 7.32412 12.1868 8.63653C12.2291 10.711 12.2715 12.7855 12.3138 15.4526ZM9.68898 12.9548C9.73132 12.7855 9.73132 12.6584 9.73132 12.5314C9.73132 11.219 9.77365 9.90661 9.73132 8.63653C9.68898 7.78982 9.56198 6.9431 9.43497 6.13872C9.3503 5.67302 9.09628 5.20733 8.92694 4.74163C8.58825 3.89491 7.36051 3.6409 6.6408 4.2336C6.13277 4.65696 5.70941 5.12265 5.45539 5.71536C5.07437 6.51974 4.73568 7.36646 4.48167 8.21318C3.84663 10.6263 3.55028 13.0818 3.3386 15.5373C3.12692 18.1198 2.99991 20.7446 2.91524 23.3694C2.83056 25.2322 2.8729 27.1373 3.12692 29.0001C3.21159 29.6775 3.38093 30.3549 3.67728 30.9899C4.524 32.5987 5.92109 32.768 7.06416 31.3709C7.57219 30.7782 7.86854 30.1008 8.12255 29.3811C8.58825 28.1111 8.84226 26.7986 9.01161 25.4862C9.09628 24.9359 9.13862 24.3855 9.18095 23.8351C8.58825 23.5388 7.99555 23.3271 7.48751 23.0307C5.75174 22.057 4.94736 20.5753 5.1167 18.5431C5.20138 17.3154 5.54006 16.1723 6.1751 15.0716C6.97948 13.8015 7.99555 12.9125 9.68898 12.9548Z"
-                    fill="#111111"
-                  />
-                  <path
-                    d="M19.2992 23.2424C19.4685 19.9826 19.7226 16.6804 20.1459 13.4628C20.4423 11.346 20.6963 9.27157 21.1196 7.19711C21.3313 5.96936 21.7547 4.78396 22.178 3.59855C22.3897 2.92118 22.7707 2.28614 23.1518 1.69344C23.4481 1.27008 23.8291 0.93139 24.2525 0.592703C24.6759 0.21168 25.2262 0 25.7766 0C27.5547 0.0846718 29.2481 0.381023 30.2219 2.15913C30.7299 3.09052 31.1532 4.02191 31.3649 5.03797C31.7883 6.94309 32 8.89054 32 10.838C32 15.2833 31.6189 19.6862 30.4759 24.0045C30.0102 25.8249 29.3328 27.5183 28.2321 29.0001C27.6817 29.7198 27.089 30.3549 26.3693 30.9052C23.6175 32.9373 20.1882 31.0322 19.5532 28.3227C19.4262 27.6877 19.3415 27.0527 19.3415 26.4176C19.2992 25.4016 19.2992 24.3008 19.2992 23.2424ZM29.3328 12.4891C29.3751 12.1504 29.4175 11.9387 29.3751 11.727C29.2481 9.99128 29.2058 8.2555 28.9941 6.51973C28.9094 5.67301 28.6131 4.82629 28.3591 4.02191C28.1051 3.21753 27.47 2.66716 26.708 2.32848C26.2846 2.1168 25.9036 2.20147 25.6072 2.54015C25.3956 2.79417 25.1839 3.09052 25.0145 3.38687C24.7605 3.97958 24.5065 4.57228 24.3372 5.16498C23.8291 6.81608 23.5751 8.55186 23.3211 10.2876C22.8131 13.8862 22.3897 17.5271 22.178 21.168C22.051 23.0307 22.0087 24.9359 21.924 26.7986C21.924 27.222 21.9664 27.6877 22.0934 28.0687C22.5167 29.3811 23.9138 29.8045 25.0145 28.9578C25.2686 28.7884 25.5226 28.5344 25.6919 28.2804C26.0729 27.7724 26.454 27.2643 26.7503 26.6716C27.47 25.3169 27.851 23.8351 28.1897 22.311C28.2321 22.184 28.1897 22.057 28.1897 21.8877C27.851 21.803 27.5547 21.7183 27.216 21.6337C26.5386 21.4643 25.9036 21.168 25.3532 20.7023C24.2948 19.8555 23.7868 18.7548 23.9138 17.4001C24.0408 15.749 24.8029 14.3519 26.0729 13.2511C26.835 12.6161 27.6817 12.2774 28.6978 12.3621C28.9094 12.4891 29.0788 12.4891 29.3328 12.4891Z"
-                    fill="#111111"
-                  />
-                </svg>
-
-                <h1
-                  className={`ml-[12px] ${passenger.className} text-[24px] text-[#111]`}
-                >
-                  Akshith Garapati
-                </h1>
-              </div>
-
-              <div
-                className={`${duplet.className} mt-[26px] w-[332px] text-[16px] leading-6 text-[#111]`}
-              >
-                <p>
-                  I’m a comp sci (‘28) freshman{" "}
-                  <span className="font-semibold">@purdue</span> that’s
-                  currently tryna understand{" "}
-                  <span className="italic">
-                    why people work the way they do
-                  </span>
-                  .
-                </p>
-
-                <br />
-
-                <p>
-                  If you’ve ever even written down a simple todo list or are the
-                  kind of person that has 30 zapier integrations to hold your
-                  life together,{" "}
-                  <span className="underline">i want to talk to you</span>.
-                </p>
-
-                <br />
-
-                <p>
-                  I use this place to write in an effort to teach myself,
-                  experiment with new pieces of web tech, train my design
-                  muscles or overcome the twitter character limit.
-                </p>
-
-                <br />
-
-                <p>
-                  I've previously built organizational systems for teams{" "}
-                  <span className="font-semibold">@dimension</span>, goofed
-                  around with some crazy smart ppl{" "}
-                  <span className="font-semibold">@tks</span> and got my hs to
-                  agree to hosting an irl hackathon on campus with ~85 ppl
-                  participating over the weekend.
-                </p>
-
-                <br />
-
-                <p>
-                  I get my hands dirty quite often with projects that’ve
-                  included building a gen-z focused newsletter, a crypto
-                  UPI-like payment system, a socialized traveling web app, a
-                  linktree replacement amongst many other random chronicles in
-                  between.
-                </p>
-              </div>
-
-              <div className="mt-[24px]">
-                <h1 className={`${duplet.className} text-[16px] text-[#999]`}>
-                  listening to self control • frank ocean
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative ml-[20%] flex w-[40%] items-center justify-center">
-          <h1 className="text-[200px]">🍃</h1>
-          <h1
-            className={`${erika.className} absolute bottom-[20%] left-[25%] w-[145px] leading-6`}
-          >
-            note: work in progress because i’m not really sure what to put here?
-            got ideas? <span className="underline">pls tell me 🙏</span>
-          </h1>
-        </div>
       </div>
     </body>
   );
